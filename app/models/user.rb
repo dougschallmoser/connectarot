d4980680ccb7349557b788c2ef2d07c3a367d693
Doug Schallmoser <dougschallmoser@mgail.com>
2020-09-12 14:17:26 -0700

    create a method to find or create a user instance by omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,17 @@ class User < ApplicationRecord
 
     has_secure_password
     validates :name, :email, :password, presence: true
+    validates :email, uniqueness: true
 
     has_many :entries
     has_many :cards, through: :entries
+
+    def self.find_or_create_by_omniauth(auth_hash)
+        self.find_or_create_by(email: auth_hash['info']['email']) do |u| 
+            u.name = auth_hash['info']['name']
+            u.email = auth_hash['info']['email']
+            u.password = SecureRandom.hex
+        end
+    end
     
 end
