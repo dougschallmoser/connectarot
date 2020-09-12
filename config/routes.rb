8fe4b0b314d51105153125d4c9c858a8e0665a9a
Doug Schallmoser <dougschallmoser@mgail.com>
2020-09-12 11:33:42 -0700

    create controllers, configure routes and create welcome index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  root 'welcome#index'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+
+  resources :users do
+    resources :entries
+  end
+
 end
