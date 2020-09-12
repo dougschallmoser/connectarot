401fafc38915e88c9206a39331a7c49740a4fe3e
Doug Schallmoser <dougschallmoser@mgail.com>
2020-09-12 12:25:47 -0700

    build create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,12 @@ class SessionsController < ApplicationController
 
     def create
         user = User.find_by(email: params[:user][:email])
-        if user 
-            
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        else
+            redirect_to login_path
+        end
     end
 
     def destroy
