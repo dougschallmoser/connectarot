8ab4613fd994b30cb1cb79bd202c5f2f8d098fb6
Doug Schallmoser <dougschallmoser@mgail.com>
2020-09-12 14:18:59 -0700

    implement if statement in create action to login with omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,22 @@ class SessionsController < ApplicationController
     end
 
     def create
-        user = User.find_by(email: params[:user][:email])
-        if user && user.authenticate(params[:user][:password])
-            session[:user_id] = user.id
-            redirect_to user_path(user)
-        else
-            redirect_to login_path
+        if auth_hash
+            user = User.find_or_create_by_omniauth(auth_hash)
+            if user.email
+                session[:user_id] = user.id
+                redirect_to user_path(user)
+            else # email is nil
+                redirect_to login_path 
+            end
+        else 
+            user = User.find_by(email: params[:user][:email])
+            if user && user.authenticate(params[:user][:password])
+                session[:user_id] = user.id
+                redirect_to user_path(user)
+            else
+                redirect_to login_path
+            end
         end
     end
 
@@ -17,5 +27,12 @@ class SessionsController < ApplicationController
         session.delete :user_id
         redirect_to root_path
     end
+
+    private
+
+    def auth_hash
+        request.env['omniauth.auth']
+    end
+
 
 end
