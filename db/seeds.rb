bb61f209dbde9aa2b300420b2a929b7db419b19d
Doug Schallmoser <dougschallmoser@mgail.com>
2020-09-13 11:13:58 -0700

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,11 @@
 ## SEED DATA ##
 
-# t.string "name"
-# t.string "type"
-# t.boolean "court"
-# t.integer "number"
-# t.string "suit"
-# t.string "meaning"
-# t.datetime "created_at", precision: 6, null: false
-# t.datetime "updated_at", precision: 6, null: false
+# CARDS
 
-one = Card.create(name: "The Fool", type: "Major", court: false, number: 0, suit: nil, meaning: "The Fool represents new beginnings, having faith in the future, being inexperienced, not knowing what to expect, having beginner's luck, improvisation and believing in the universe.")
+one = Card.create(name: "The Fool", designation: "Major", court: false, number: 0, suit: nil, meaning: "The Fool represents new beginnings, having faith in the future, being inexperienced, not knowing what to expect, having beginner's luck, improvisation and believing in the universe.")
 
-two = Card.create(name: "The Magician", type: "Major", court: false, number: 1, suit: nil, meaning: "When the Magician appears in a spread, it points to the talents, capabilities and resources at the querent's disposal to succeed. The message is to tap into one's full potential rather than holding back, especially when there is a need to transform something.")
+two = Card.create(name: "The Magician", designation: "Major", court: false, number: 1, suit: nil, meaning: "When the Magician appears in a spread, it points to the talents, capabilities and resources at the querent's disposal to succeed. The message is to tap into one's full potential rather than holding back, especially when there is a need to transform something.")
 
-three = Card.create(name: "The Magician", type: "Major", court: false, number: 1, suit: nil, meaning: "High Priestess is a card of mystery, stillness and passivity. This card suggests that it is time to retreat and reflect upon the situation and trust your inner instincts to guide you through it. Things around you are not what they appear to be right now.")
+three = Card.create(name: "The High Priestess", designation: "Major", court: false, number: 2, suit: nil, meaning: "High Priestess is a card of mystery, stillness and passivity. This card suggests that it is time to retreat and reflect upon the situation and trust your inner instincts to guide you through it. Things around you are not what they appear to be right now.")
+
+four = Card.create(name: "The Empress", designation: "Major", court: false, number: 3, suit: nil, meaning: "The Empress is traditionally associated with maternal influence, it is the card if you are hoping to start a family. She can represent the creation of life, romance, art, or new business.")
