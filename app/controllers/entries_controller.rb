a7fe5da2bfb9f289ed976e557b5ee34c65e1fb7c
Doug Schallmoser <dougschallmoser@mgail.com>
2020-09-13 13:18:35 -0700

    add functionality to randomly generate card when submit button is clicked

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,11 +1,32 @@
 class EntriesController < ApplicationController
 
+    def show
+        @user = User.find_by(id: params[:user_id])
+        @entry = Entry.find_by(id: params[:id])
+    end
+
     def new
         @user = User.find_by(id: params[:user_id])
         @entry = @user.entries.build
     end
 
     def create
+        if params[:entry_id].present?
+            @entry = Entry.find_by(id: params[:entry_id])
+        else
+            @entry = Entry.create(user_id: params[:user_id])
+        end
+        @random_card = Card.randomize
+        until !@entry.cards.include?(@random_card) do
+            @random_card = Card.randomize
+        end
+        @entry.cards << @random_card
+        redirect_to user_entry_path(@entry.user, @entry)
+    end
+
+    private
+
+    def entry_params
     end
 
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -1,5 +1,7 @@
 class Card < ApplicationRecord
 
+    scope :randomize, -> {all.sample}
+
     has_many :entries_cards
     has_many :entries, through: :entries_cards
     has_many :users, through: :entries
