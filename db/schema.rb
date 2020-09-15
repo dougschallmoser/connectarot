34e96b1edfd3d4e8c5d6caabef3d51816ca13fdc
Doug Schallmoser <dougschallmoser@mgail.com>
2020-09-14 17:31:07 -0700

    add image urls to seed file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_13_224726) do
+ActiveRecord::Schema.define(version: 2020_09_14_233337) do
 
   create_table "cards", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_09_13_224726) do
     t.string "meaning"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
   end
 
   create_table "categories", force: :cascade do |t|
