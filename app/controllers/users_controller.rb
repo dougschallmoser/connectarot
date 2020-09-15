a79c908796551d8a1e84c0d6958b4384d17e7412
Doug Schallmoser <dougschallmoser@mgail.com>
2020-09-15 09:19:51 -0700

    add error messsages when user signs up, add error styling:

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,12 @@ class UsersController < ApplicationController
     end
 
     def create
-        user = User.new(user_params)
-        if user.save
-            session[:user_id] = user.id 
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id 
             redirect_to user_entries_path(user)
         else
-            redirect_to new_user_path
+            render :new
         end
     end
 
