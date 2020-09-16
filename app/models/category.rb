f9586b1ec98640ee5a579174f87b73932154f53e
Doug Schallmoser <dougschallmoser@mgail.com>
2020-09-16 11:12:34 -0700

    reorganize entry show page

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
 
     has_many :entries
-    # validates :question_1, :question_2, :question_3, presence: true
+    validates :question_1, :question_2, :question_3, presence: true
     
 end
