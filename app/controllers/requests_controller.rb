ded13add4d0f4a6809ea25de7b53f79f59e725ed
Doug Schallmoser <dougschallmoser@mgail.com>
2020-09-16 16:24:31 -0700

    implement create action logic

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -3,11 +3,26 @@ class RequestsController < ApplicationController
     before_action :require_login
 
     def index
+        @requests = Request.all
     end
     
     def new
         @request = Request.new
     end
 
+    def create
+        @request = Request.new(request_params)
+        if @request.save
+            redirect_to requests_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def request_params
+        params.require(:request).permit(:title, :requestor_id)
+    end
 
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -2,5 +2,7 @@ class Request < ApplicationRecord
 
     belongs_to :requestor, class_name: "User"
     belongs_to :responder, class_name: "User", optional: true
+
+    validates :title, presence: true
     
 end
