db5d459e9710afb7dc4806f5848174158a0b6bf4
Doug Schallmoser <dougschallmoser@mgail.com>
2020-09-18 13:21:44 -0700

    update

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,10 +13,10 @@ class SessionsController < ApplicationController
                 redirect_to login_path
             end
         else 
-            user = User.find_by(email: params[:user][:email])
-            if user && user.authenticate(params[:user][:password])
-                session[:user_id] = user.id
-                redirect_to user_entries_path(user)
+            @user = User.find_by(email: params[:user][:email])
+            if @user && @user.authenticate(params[:user][:password])
+                session[:user_id] = @user.id
+                redirect_to user_entries_path(@user)
             else
                 redirect_to login_path
             end
@@ -33,6 +33,5 @@ class SessionsController < ApplicationController
     def auth_hash
         request.env['omniauth.auth']
     end
-
 
 end
