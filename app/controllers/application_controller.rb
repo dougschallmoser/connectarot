3562e7ed2deb478664656a4d00c4094edd3f53b3
Doug Schallmoser <dougschallmoser@mgail.com>
2020-09-21 09:25:39 -0700

    remove authenticate and update info method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,6 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user, :logged_in?
 
-  def authenticate_and_update_info(user)
-    if user.authenticate(params[:user][:password])
-      user.update(user_params)
-      if params[:user][:new_password].present?
-        user.password = params[:user][:new_password]
-        user.save
-      end
-    else
-        # message
-    end
-    redirect_to edit_user_path(user)
-  end
-
   private
 
   def current_user
