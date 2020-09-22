b30c8f6741ad704b7ca6fbcff564a23e6284c765
Doug Schallmoser <dougschallmoser@mgail.com>
2020-09-22 13:20:05 -0700

    add helper method to display specific designation of card, clean up view

--- app/helpers/cards_helper.rb
+++ app/helpers/cards_helper.rb
@@ -1,7 +1,15 @@
 module CardsHelper
-
   def display_card_number_if_major(card)
     " (#{card.number})" if card.designation == "Major"
   end
 
+  def display_link_by_designation(card, designation)
+    if card.designation == designation 
+      content_tag(:tr) do 
+        content_tag(:td) do
+          concat link_to(card.name, card_path(card))
+        end
+      end
+    end
+  end
 end
