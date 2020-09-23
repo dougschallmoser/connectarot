8147b76dae8825d06dfae39cb28019563a5a54a9
Doug Schallmoser <dougschallmoser@mgail.com>
2020-09-23 09:22:00 -0700

    change logout to a post request instead of delete, add confirmation when clicking logout

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  post '/logout', to: 'sessions#destroy'
 
   resources :users, except: [:index, :show] do
     resources :entries, shallow: true do 
