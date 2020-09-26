0312c1f824a0e5377379fd8e519922ed00060db3
Doug Schallmoser <dougschallmoser@mgail.com>
2020-09-25 19:15:21 -0700

    create two methods for the create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,25 +4,9 @@ class SessionsController < ApplicationController
 
   def create
     if auth_hash
-      user = User.find_or_create_by_omniauth(auth_hash)
-      if user.email
-        session[:user_id] = user.id
-        flash[:message] = "You have successfully authenticated"
-        redirect_to user_entries_path(user)
-      else
-        flash[:message] = "Email is is blank. Please try again"
-        redirect_to login_path
-      end
+      login_with_omniauth
     else 
-      @user = User.find_by(email: params[:user][:email])
-      if @user && @user.authenticate(params[:user][:password])
-        session[:user_id] = @user.id
-        flash[:message] = "You have successfully logged in"
-        redirect_to user_entries_path(@user)
-      else
-        flash[:message] = "Invalid credentials. Please try again"
-        render :new
-      end
+      login_with_account
     end
   end
 
@@ -35,5 +19,29 @@ class SessionsController < ApplicationController
 
   def auth_hash
     request.env['omniauth.auth']
+  end
+
+  def login_with_omniauth
+    user = User.find_or_create_by_omniauth(auth_hash)
+    if user.email
+      session[:user_id] = user.id
+      flash[:message] = "You have successfully authenticated"
+      redirect_to user_entries_path(user)
+    else
+      flash[:message] = "Email is is blank. Please try again"
+      redirect_to login_path
+    end
+  end
+
+  def login_with_account
+    @user = User.find_by(email: params[:user][:email])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      flash[:message] = "You have successfully logged in"
+      redirect_to user_entries_path(@user)
+    else
+      flash[:message] = "Invalid credentials. Please try again"
+      render :new
+    end
   end
 end
