c56587c5d892624073c8ec9578e1c83b49547c86
Doug Schallmoser <dougschallmoser@mgail.com>
2020-09-25 19:15:04 -0700

    rearrange routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,15 @@
 Rails.application.routes.draw do
   root 'welcome#index'
-
   get '/signup', to: 'users#new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   post '/logout', to: 'sessions#destroy'
-
+  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
+  resources :cards, only: [:show, :index]
+  resources :requests, except: [:edit]
   resources :users, except: [:new] do
     resources :entries, shallow: true do 
       resources :thoughts, only: [:create, :destroy]
     end
   end
-
-  resources :cards, only: [:show, :index]
-  resources :requests, except: [:edit]
-
-  match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
 end
