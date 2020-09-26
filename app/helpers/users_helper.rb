1c35610bf5e91aa08086187f3a128d44008e3514
Doug Schallmoser <dougschallmoser@mgail.com>
2020-09-26 09:44:47 -0700

    refactor if statement to one line

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,5 @@
 module UsersHelper
   def display_user_photo(user)
-    if user.image
-      image_tag(user.image, width: "200", id: "profile-pic")
-    end
+    image_tag(user.image, width: "200", id: "profile-pic") if user.image
   end
 end
