49822962a0feb873e3981e094b60852a1f96081a
Doug Schallmoser <dougschallmoser@mgail.com>
2020-09-25 18:15:46 -0700

    reorders associations and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
-  has_secure_password
-  validates :name, :email, :password, presence: true
-  validates :email, uniqueness: true
   has_many :entries
   has_many :cards, through: :entries
   has_many :requests, class_name: "Request", foreign_key: "requestor_id"
   has_many :responses, class_name: "Request", foreign_key: "responder_id"
+  validates :name, :email, :password, presence: true
+  validates :email, uniqueness: true
+  has_secure_password
 
   def self.find_or_create_by_omniauth(auth_hash)
     self.find_or_create_by(email: auth_hash['info']['email']) do |u| 
