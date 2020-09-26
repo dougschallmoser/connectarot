a0e5f579df28a4129776bfd125794bd59225ede5
Doug Schallmoser <dougschallmoser@mgail.com>
2020-09-25 18:43:32 -0700

    add minimum length to desription validation

--- app/models/request.rb
+++ app/models/request.rb
@@ -3,7 +3,7 @@ class Request < ApplicationRecord
   belongs_to :responder, class_name: "User", optional: true
   has_one :entry
   validates :title, presence: true, length: { minimum: 3 }
-  validates :description, presence: true
+  validates :description, presence: true, length: { minimum: 4 }
   validate :different_responder
   scope :all_open_requests, -> { order(:responder_id) }
   scope :by_recent , -> { order(created_at: :desc) }
