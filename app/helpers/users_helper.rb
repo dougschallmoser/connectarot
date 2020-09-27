1581df9c014f2245b4406994112cc305e3d43d01
Doug Schallmoser <dougschallmoser@mgail.com>
2020-09-26 23:03:05 -0700

    remove width from content_tag

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,5 @@
 module UsersHelper
   def display_user_photo(user)
-    image_tag(user.image, width: "200", id: "profile-pic") if user.image
+    image_tag(user.image, id: "profile-pic") if user.image
   end
 end
