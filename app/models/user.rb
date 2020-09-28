d55aa020801ccd6f0dd97aa227c0a11c1991321f
Doug Schallmoser <dougschallmoser@mgail.com>
2020-09-28 13:25:44 -0700

    remove password presence validation, remove assignment of email in block

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,13 @@ class User < ApplicationRecord
   has_many :cards, through: :entries
   has_many :requests, class_name: "Request", foreign_key: "requestor_id"
   has_many :responses, class_name: "Request", foreign_key: "responder_id"
-  validates :name, :email, :password, presence: true
+  validates :name, :email, presence: true
   validates :email, uniqueness: true
   has_secure_password
 
   def self.find_or_create_by_omniauth(auth_hash)
     self.find_or_create_by(email: auth_hash['info']['email']) do |u| 
       u.name = auth_hash['info']['name']
-      u.email = auth_hash['info']['email']
       u.image = auth_hash['info']['image']
       u.password = SecureRandom.hex
     end
